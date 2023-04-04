37b4a1fab09721f8224ebb14695ece03aed20d34
Gabriel2897 <126614206+Gabriel2897@users.noreply.github.com>
2023-04-04 20:59:53 -0300

    commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function saludar() {
+    respuesta = prompt("¡Hola Mundo! ¿Como estan?");
+    if (respuesta == "muy bien") {
+        alert("Exelente")
+    } else if (respuesta == "bien") {
+        alert("Me alegro")
+    } else {
+        alert("Que mal")
+    }
+    console.log(saludar);
+}
+saludar()
+
+function miedo() {
+    respuesta = prompt(" ¿Tienen miedo?");
+    if (respuesta == "no") {
+        alert("Que bien, por que yo si")
+    } else if (respuesta == "si") {
+        alert("Yo tambien Yei, Comencemos")
+    }
+}
+miedo()
+
+
+alert("Pagina en contruccion")
+
